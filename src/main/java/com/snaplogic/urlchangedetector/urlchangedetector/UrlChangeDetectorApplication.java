a8b1f0a81286e29f7fff7099f23a7504320ce019
GANESH BABU NELAMANGALA <ganesh.sit@gmail.com>
2017-09-18 22:54:13 -0500

    project skeleton using spring

---
+++ src/main/java/com/snaplogic/urlchangedetector/urlchangedetector/UrlChangeDetectorApplication.java
@@ -0,0 +1,12 @@
+package com.snaplogic.urlchangedetector.urlchangedetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlChangeDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlChangeDetectorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/snaplogic/urlchangedetector/urlchangedetector/UrlChangeDetectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.snaplogic.urlchangedetector.urlchangedetector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UrlChangeDetectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
